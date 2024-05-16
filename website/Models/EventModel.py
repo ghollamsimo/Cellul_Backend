b77a5b8ed59a91f01efc2f2ea2cb37f5f88541a1
ghollamsimo <ghollamsimo1@gmail.com>
2024-05-16 20:02:29 +0100

    Adding The Field Of admin_id In Table Event

--- website/Models/EventModel.py
+++ website/Models/EventModel.py
@@ -8,7 +8,7 @@ from website.Models.AdminModel import Admin
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    admin = models.ForeignKey(Admin, on_delete=models.CASCADE).null
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(auto_now=True)
     localisation = models.CharField(max_length=200)
